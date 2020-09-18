ab4e49f0d26794afb131bc8e25a4d06839148c6e
chipo <pnapoli33@gmail.com>
2020-09-18 02:20:46 -0300

    Add @PutMapping for update a certain ServiceBudget.

--- src/main/java/com/insside/budget/service_budget/ServiceBudgetController.java
+++ src/main/java/com/insside/budget/service_budget/ServiceBudgetController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class ServiceBudgetController {
 	
 	@PostMapping("/services")
 	public void createServiceBudget(@RequestBody ServiceBudget serviceBudget) {
+		serviceBudgetService.save(serviceBudget);
+	}
+	
+	@PutMapping("/services")
+	public void updateServiceBudget(@RequestBody ServiceBudget serviceBudget) {
 		serviceBudgetService.save(serviceBudget);
 	}
 	
